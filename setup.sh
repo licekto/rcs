612a4eb402ad854bec1bd6b0cbd87a77f640c360
Tomas Licek <licekto@gmail.com>
2022-06-10 12:20:05 +0200

    Create mc config dir if does not exist

--- setup.sh
+++ setup.sh
@@ -6,6 +6,7 @@ rm -rf $HOME_DIR/.gitconfig $HOME_DIR/.vimrc $HOME_DIR/.config/mc/ini
 
 ln -s $HOME_DIR/.rcs/gitconfig $HOME_DIR/.gitconfig
 ln -s $HOME_DIR/.rcs/vimrc $HOME_DIR/.vimrc
+mkdir -p $HOME_DIR/.config/mc
 ln -s $HOME_DIR/.rcs/mc_ini $HOME_DIR/.config/mc/ini
 git config --global core.excludesfile $HOME_DIR/.rcs/gitignore
 
